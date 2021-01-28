3d4ee62ba06fd226dfe2f73431913a715c3b49e2
Xiaoyuer-6 <74359600+Xiaoyuer-6@users.noreply.github.com>
2021-01-28 23:45:32 +0800

    Update ScoreComparator.java

--- src/常见的3个接口/ScoreComparator.java
+++ src/常见的3个接口/ScoreComparator.java
@@ -2,13 +2,7 @@ package 常见的3个接口;
 
 import java.util.Comparator;
 
-/**
- * Created with IntelliJ IDEA.
- * Description:
- * User: GaoBo
- * Date: 2021-01-17
- * Time: 11:55
- */
+
 public class ScoreComparator implements Comparator<Student2> {
     @Override
     public int compare(Student2 o1, Student2 o2) {
