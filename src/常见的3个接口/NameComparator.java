4a7dc8324acb9a8d64a1522c8a8bd92a52f23ec5
Xiaoyuer-6 <74359600+Xiaoyuer-6@users.noreply.github.com>
2021-01-28 23:44:39 +0800

    Update NameComparator.java

--- src/常见的3个接口/NameComparator.java
+++ src/常见的3个接口/NameComparator.java
@@ -2,13 +2,7 @@ package 常见的3个接口;
 
 import java.util.Comparator;
 
-/**
- * Created with IntelliJ IDEA.
- * Description:
- * User: GaoBo
- * Date: 2021-01-17
- * Time: 11:59
- */
+
 public class NameComparator implements Comparator<Student2> {
 
     @Override
